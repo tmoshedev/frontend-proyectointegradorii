e75ac6b46686c23d2ece897a4e524cc5802f328d
tmoshedev <angelfabian123@hotmail.com>
2025-10-28 19:41:29 -0500

    fix: actualizar URL de descarga de PDF a la nueva dirección del backend

--- src/pages/Lead/components/lead-buyer.component.tsx
+++ src/pages/Lead/components/lead-buyer.component.tsx
@@ -51,7 +51,7 @@ export const LeadBuyerComponent = ({ changeHistorialView }: Props) => {
       return;
     }
     try {
-      const url = `http://backend-proyectointegradorii.test/pdf-templates/${selectedPdfTemplate}/export/${lead.uuid}`;
+      const url = `https://backend-demo-crm.alitorres.com.pe/pdf-templates/${selectedPdfTemplate}/export/${lead.uuid}`;
       const response = await fetch(url, { method: 'GET' });
       if (!response.ok) throw new Error('Error al generar el PDF');
       const blob = await response.blob();
